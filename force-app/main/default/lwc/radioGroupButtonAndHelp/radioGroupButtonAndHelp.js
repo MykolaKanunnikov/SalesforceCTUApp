d2fc94d5279a60458bdd3aba7766eb4c6a0d7b0b
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-06 13:48:31 +0200

    add save error handling

--- force-app/main/default/lwc/radioGroupButtonAndHelp/radioGroupButtonAndHelp.js
+++ force-app/main/default/lwc/radioGroupButtonAndHelp/radioGroupButtonAndHelp.js
@@ -1,6 +1,7 @@
 import { NavigationMixin } from 'lightning/navigation'
 import getFileRecordId from '@salesforce/apex/ShipmentController.getFileRecordId';
 import updateRadioValue from '@salesforce/apex/ShipmentController.updateRadioValue';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 // define radio group 
 const options = () => {
@@ -38,7 +39,29 @@ function radioChange(event) {
     let value = event.target.value;
     let valueId = event.target.dataset.id;
     if (this.currentChecklistId) {
-        updateRadioValue({ currentChecklistId: this.currentChecklistId, valueId: valueId, value: value });
+        updateRadioValue({ currentChecklistId: this.currentChecklistId, valueId: valueId, value: value })
+            .then(resp => {
+                if (resp.isSuccess) {
+                    console.log('success');
+                } else {
+                    const errorEvent = new ShowToastEvent({
+                        title: 'Save error',
+                        variant: 'error',
+                        mode: 'sticky',
+                        message: resp.responseObj
+                    });
+                    this.dispatchEvent(errorEvent);
+                }
+            })
+            .catch(error => {
+                const noResponseEvent = new ShowToastEvent({
+                    title: 'Save error',
+                    variant: 'error',
+                    mode: 'sticky',
+                    message: 'No response / saving: ' + error.body.message
+                });
+                this.dispatchEvent(noResponseEvent);
+            })
     }
 }
 
