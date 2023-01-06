880883506f0f3463c6e3584d78d352a7249cd263
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-06 17:58:45 +0200

    1. error handling on help + 2. more concise js

--- force-app/main/default/lwc/packingArea1to7/packingArea1to7.js
+++ force-app/main/default/lwc/packingArea1to7/packingArea1to7.js
@@ -1,49 +1,22 @@
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation'
-import getCurrentChecklistId from '@salesforce/apex/ShipmentController.getCurrentChecklistId';
-import getChecklistValues from '@salesforce/apex/ShipmentController.getChecklistValues';
-import { openReference, radioChange, options } from 'c/radioGroupButtonAndHelp';
-import {packingAreaQuestions} from 'c/questions';
+import { openReference, radioChange, options, checklistSetup } from 'c/radioGroupButtonAndHelp';
+import { packingAreaQuestions } from 'c/questions';
 
 export default class PackingArea1to7 extends NavigationMixin(LightningElement) {
-
-    packingAreaQuestions = packingAreaQuestions; 
 
     @api recordId;
 
     currentChecklistId;
 
-    value1 = '';
-    value2 = '';
-    value3 = '';
-    value4 = '';
-    value5 = '';
-    value6 = '';
-    value7 = '';
+    packingAreaQuestions = packingAreaQuestions;
 
-    // 1. define apropriate checklist when a component is inserted to the DOM
-    // 2. retrieve already inserted values
+    values = '';
+
+    // set values and currentChecklistId fields
     connectedCallback() {
-        // 1
         const fields = 'packingArea1__c, packingArea2__c, packingArea3__c, packingArea4__c, packingArea5__c, packingArea6__c, packingArea7__c';
-        if (!this.currentChecklistId) {
-            getCurrentChecklistId({ recordId: this.recordId })
-                .then(result => {
-                    this.currentChecklistId = result;
-
-                    // 2
-                    getChecklistValues({ currentChecklistId: result, fields: fields })
-                        .then(values => {
-                            this.value1 = values.packingArea1__c;
-                            this.value2 = values.packingArea2__c;
-                            this.value3 = values.packingArea3__c;
-                            this.value4 = values.packingArea4__c;
-                            this.value5 = values.packingArea5__c;
-                            this.value6 = values.packingArea6__c;
-                            this.value7 = values.packingArea7__c;
-                        })
-                });
-        }
+        checklistSetup.call(this, fields);
     }
 
     // define radio group settings 
@@ -61,7 +34,5 @@ export default class PackingArea1to7 extends NavigationMixin(LightningElement) {
     handleChange(event) {
         radioChange.call(this, event);
     }
-
-
 
 }
--- force-app/main/default/lwc/radioGroupButtonAndHelp/radioGroupButtonAndHelp.js
+++ force-app/main/default/lwc/radioGroupButtonAndHelp/radioGroupButtonAndHelp.js
@@ -1,7 +1,9 @@
-import { NavigationMixin } from 'lightning/navigation'
+import { NavigationMixin } from 'lightning/navigation';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getFileRecordId from '@salesforce/apex/ShipmentController.getFileRecordId';
 import updateRadioValue from '@salesforce/apex/ShipmentController.updateRadioValue';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import getCurrentChecklistId from '@salesforce/apex/ShipmentController.getCurrentChecklistId';
+import getChecklistValues from '@salesforce/apex/ShipmentController.getChecklistValues';
 
 // define radio group 
 const options = () => {
@@ -12,26 +14,56 @@ const options = () => {
     ];
 };
 
+// 1. define apropriate checklist
+// 2. retrieve already inserted values
+function checklistSetup(fields) {
+    if (!this.currentChecklistId) {
+        getCurrentChecklistId({ recordId: this.recordId })
+            .then(result => {
+                this.currentChecklistId = result;
+                // 2
+                getChecklistValues({ currentChecklistId: result, fields: fields })
+                    .then(values => {
+                        this.values = values;
+                    })
+            });
+    }
+
+}
+
 // display a relevant reference file    
 function openReference(event) {
     let itemId = event.target.dataset.id;
     let itemTitle = `${itemId}.pdf`;
     getFileRecordId({ title: itemTitle })
-        .then(properPdfId => {
-            try {
+        .then(resp => {
+            if (resp.isSuccess) {
                 this[NavigationMixin.Navigate]({
                     type: "standard__namedPage",
                     attributes: {
                         pageName: 'filePreview'
                     },
                     state: {
-                        selectedRecordId: properPdfId
+                        selectedRecordId: resp.responseObj
                     }
                 });
-            } catch (error) {
-                console.error(error);
+            } else {
+                const errorEvent = new ShowToastEvent({
+                    title: 'File error',
+                    variant: 'error',
+                    message: resp.responseObj
+                });
+                this.dispatchEvent(errorEvent);
             }
-        });
+        })
+        .catch(error => {
+            const errorEvent = new ShowToastEvent({
+                title: 'File error',
+                variant: 'error',
+                message: "Error: " + error.body.message
+            });
+            this.dispatchEvent(errorEvent);
+        })
 }
 
 // save values from a radio button to the database
@@ -41,13 +73,10 @@ function radioChange(event) {
     if (this.currentChecklistId) {
         updateRadioValue({ currentChecklistId: this.currentChecklistId, valueId: valueId, value: value })
             .then(resp => {
-                if (resp.isSuccess) {
-                    console.log('success');
-                } else {
+                if (!resp.isSuccess) {
                     const errorEvent = new ShowToastEvent({
                         title: 'Save error',
                         variant: 'error',
-                        mode: 'sticky',
                         message: resp.responseObj
                     });
                     this.dispatchEvent(errorEvent);
@@ -57,7 +86,6 @@ function radioChange(event) {
                 const noResponseEvent = new ShowToastEvent({
                     title: 'Save error',
                     variant: 'error',
-                    mode: 'sticky',
                     message: 'No response / saving: ' + error.body.message
                 });
                 this.dispatchEvent(noResponseEvent);
@@ -65,4 +93,4 @@ function radioChange(event) {
     }
 }
 
-export { openReference, radioChange, options };
+export { openReference, radioChange, options, checklistSetup };
