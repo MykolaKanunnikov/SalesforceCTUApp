9d3be3198ebc074b923eebab256aa27a3ac9d5b4
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-07 16:22:23 +0200

    ClosingContainer28to29

---
+++ force-app/main/default/lwc/closingContainer28to29/closingContainer28to29.js
@@ -0,0 +1,36 @@
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+import { openReference, radioChange, options, checklistSetup } from 'c/radioGroupButtonAndHelp';
+import { closingContainer } from 'c/questions';
+
+export default class ClosingContainer28to29 extends NavigationMixin(LightningElement) {
+    @api recordId;
+
+    currentChecklistId;
+
+    closingContainer = closingContainer;
+
+    values = '';
+
+    // in the following code '.call' is needed to explicitly tell the  
+    // imported utility function to use 'this' from the given component
+
+    // set values and currentChecklistId fields
+    connectedCallback() {
+        const fields = 'closingContainer28__c, closingContainer29__c';
+        checklistSetup.call(this, fields);
+    }
+
+    // define radio group settings 
+    options = options();
+
+    // display a relevant reference file.
+    handleClick(event) {
+        openReference.call(this, event);
+    }
+
+    // save values from a radio button to the database
+    handleChange(event) {
+        radioChange.call(this, event);
+    }
+}
