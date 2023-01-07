728acc64919866aed99a9387eeb1dedc0ccc29d9
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-07 16:13:54 +0200

    AfterPacking24to27

---
+++ force-app/main/default/lwc/afterPacking24to27/afterPacking24to27.js
@@ -0,0 +1,36 @@
+import { LightningElement, api } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation'
+import { openReference, radioChange, options, checklistSetup } from 'c/radioGroupButtonAndHelp';
+import { afterPacking } from 'c/questions';
+
+export default class AfterPacking24to27 extends NavigationMixin(LightningElement) {
+    @api recordId;
+
+    currentChecklistId;
+
+    afterPacking = afterPacking;
+
+    values = '';
+
+    // in the following code '.call' is needed to explicitly tell the  
+    // imported utility function to use 'this' from the given component
+
+    // set values and currentChecklistId fields
+    connectedCallback() {
+        const fields = 'afterPacking24__c, afterPacking25__c, afterPacking26__c, afterPacking27__c';
+        checklistSetup.call(this, fields);
+    }
+
+    // define radio group settings 
+    options = options();
+
+    // display a relevant reference file.
+    handleClick(event) {
+        openReference.call(this, event);
+    }
+
+    // save values from a radio button to the database
+    handleChange(event) {
+        radioChange.call(this, event);
+    }
+}
