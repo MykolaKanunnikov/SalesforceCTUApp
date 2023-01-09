634ff7f5ce6f2fcca581012995cd100db597fa3b
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-09 21:49:25 +0200

    form factor consideration

--- force-app/main/default/lwc/checklistTabset/checklistTabset.js
+++ force-app/main/default/lwc/checklistTabset/checklistTabset.js
@@ -1,7 +1,17 @@
 import { LightningElement, api } from 'lwc';
+import formFactor from '@salesforce/client/formFactor'
 
 export default class ChecklistTabset extends LightningElement {
-    
+
     @api recordId;
-    
+
+    // vertical variant isn't a good fit for small-sized phones and tablets
+    get tabsetVariant() {
+        if (formFactor === 'Large') {
+            return 'vertical';
+        } else {
+            return 'standard';
+        }
+    }
+
 }
