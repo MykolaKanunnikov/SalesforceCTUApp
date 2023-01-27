13587ff898d15036fcaf95bbbb77f5e9c92ac7ec
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-27 19:28:50 +0200

    Icons to tabset added

--- force-app/main/default/lwc/checklistTabset/checklistTabset.js
+++ force-app/main/default/lwc/checklistTabset/checklistTabset.js
@@ -1,9 +1,26 @@
 import { LightningElement, api } from 'lwc';
 import formFactor from '@salesforce/client/formFactor'
+import getIconMapObject from '@salesforce/apex/ShipmentController.getIconMapObject';
+
 
 export default class ChecklistTabset extends LightningElement {
 
     @api recordId;
+    
+    iconName = '';
+
+    renderedCallback(){
+        getIconMapObject({ recordId: this.recordId })
+        .then(resp => {
+            if (resp.isSuccess) {
+                const respObject = JSON.parse(resp.responseObj);
+                this.iconName = respObject;
+            } else {
+                console.log('Not success - ' + JSON.stringify(resp));
+            }
+        })
+
+    }
 
     // vertical variant isn't a good fit for small-sized phones and tablets
     get tabsetVariant() {
