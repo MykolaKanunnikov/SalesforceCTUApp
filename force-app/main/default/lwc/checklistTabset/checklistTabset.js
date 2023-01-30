01ad79713876060233e1b0b3eb1a9c821550e545
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-01-30 16:12:44 +0200

    positive test  on getIconMapObject()

--- force-app/main/default/lwc/checklistTabset/checklistTabset.js
+++ force-app/main/default/lwc/checklistTabset/checklistTabset.js
@@ -6,20 +6,33 @@ import getIconMapObject from '@salesforce/apex/ShipmentController.getIconMapObje
 export default class ChecklistTabset extends LightningElement {
 
     @api recordId;
-    
+
     iconName = '';
 
-    renderedCallback(){
+    // display status icons depending on answers in the checklist
+    renderedCallback() {
         getIconMapObject({ recordId: this.recordId })
-        .then(resp => {
-            if (resp.isSuccess) {
-                const respObject = JSON.parse(resp.responseObj);
-                this.iconName = respObject;
-            } else {
-                console.log('Not success - ' + JSON.stringify(resp));
-            }
-        })
-
+            .then(resp => {
+                if (resp.isSuccess) {
+                    const respObject = JSON.parse(resp.responseObj);
+                    this.iconName = respObject;
+                } else {
+                    const errorEvent = new ShowToastEvent({
+                        title: 'Status icons JS error',
+                        variant: 'error',
+                        message: resp.responseObj
+                    });
+                    this.dispatchEvent(errorEvent);
+                }
+            })
+            .catch(error => {
+                const errorEvent = new ShowToastEvent({
+                    title: 'Status icons Apex error',
+                    variant: 'error',
+                    message: "Error: " + error.body.message
+                });
+                this.dispatchEvent(errorEvent);
+            })
     }
 
     // vertical variant isn't a good fit for small-sized phones and tablets
