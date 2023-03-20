a223ebbab0b532699107e273692868720fbcc33a
Mykola Kanunnikov <kanunnikovmykola@gmail.com>
2023-03-20 18:30:46 +0200

    Jest tests on tabset LWC

---
+++ force-app/test/jest-mocks/c/packingArea1to7.js
@@ -0,0 +1,17 @@
+import { LightningElement, api } from 'lwc';
+
+export default class packingArea1to7 extends LightningElement {
+    @api recordId;
+    currentChecklistId = '';
+    packingArea = {};
+    values = '';
+    options = [];
+
+    handleClick(event) {
+        // do nothing
+    }
+
+    handleChange(event) {
+        // do nothing
+    }
+}
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,15 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
+    moduleNameMapper: {
+        '^@salesforce/client/formFactor$': '<rootDir>/force-app/test/jest-mocks/c/formFactor.json',
+        '^c/packingArea1to7$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/containerCondition8to12$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/packingContainer13to19$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/dangerousGoods20to23$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/afterPacking24to27$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/closingContainer28to29$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+        '^c/dispatchingContainer30to34$': '<rootDir>/force-app/test/jest-mocks/c/packingArea1to7',
+    },
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
